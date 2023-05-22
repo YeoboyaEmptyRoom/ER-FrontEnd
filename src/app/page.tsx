c0db8b5304702824cc9d31fa16152b479a52e82c
kangkong0805 <kang3530554@gmail.com>
2023-05-23 01:10:46 +0900

    🔥 Remove babel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
 
-
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
-
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -94,5 +93,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
