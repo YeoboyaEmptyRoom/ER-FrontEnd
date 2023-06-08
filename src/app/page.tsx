c7b033d8a980926c58d82ca1095f1f946dfbc525
kangkong0805 <kang3530554@gmail.com>
2023-06-08 10:14:55 +0900

    💄 Add : 최신 빈 방 리스트 ui 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Header from '@/components/Header/organisms';
 import Banner from '@/components/Home/molecules/Banner';
+import RoomContent from '@/components/Home/organism/RoomContent';
 
 export default function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <RoomContent />
     </>
   );
 }
---
+++ src/components/Home/atoms/RoomItem/style.ts
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+import Image from 'next/image';
+
+export const Layer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 300px;
+`;
+
+export const RoomImg = styled(Image)`
+  width: 150px;
+  height: 150px;
+  object-fit: contain;
+  border-radius: 0.5em;
+`;
+
+export const RoomInfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.5em;
+`;
