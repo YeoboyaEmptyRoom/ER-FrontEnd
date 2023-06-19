e5b2d624508ed00195ba8fb09bbecb7395a5e93f
kangkong <kang3530554@gmail.com>
2023-06-20 00:45:20 +0900

    ✨ Add : 최신 빈 방 리스트 api 추가

--- src/components/Common/atoms/RoomItem/index.tsx
+++ src/components/Common/atoms/RoomItem/index.tsx
@@ -1,15 +1,18 @@
 import * as S from './style';
 import roomImg from '../../../../../public/static/png/roomImg.png';
-
-const RoomItem = () => {
+import { RoomType } from '@/types/components/Room';
+interface Props {
+  data: RoomType;
+}
+const RoomItem = ({ data }: Props) => {
   return (
     <S.Layer>
       <S.RoomImg src={roomImg} alt="RoomImg" width="150" height="150" />
       <S.RoomInfoBox>
-        <h2>월세 500/40</h2>
-        <p>23.14m^2, 7층</p>
-        <p>구로구 구로동</p>
-        <p>분리형 원룸</p>
+        <h2>{data.rent_type}</h2>
+        <p>{data.area}</p>
+        <p>{data.location}</p>
+        <p>{data.room_type}</p>
       </S.RoomInfoBox>
     </S.Layer>
   );
--- src/components/Common/molecules/RoomList/index.tsx
+++ src/components/Common/molecules/RoomList/index.tsx
@@ -1,15 +1,27 @@
+import useFetch from '@/hook/useFetch';
 import RoomItem from '../../atoms/RoomItem';
 import { Layer } from './style';
+import { useEffect } from 'react';
+import { RoomType } from '@/types/components/Room';
 
 const RoomList = ({ columns }: { columns: number }) => {
+  const { fetch, data } = useFetch<RoomType[]>({
+    url: '/main/rooms/',
+    method: 'get',
+    onSuccess(data) {
+      console.log(data);
+    },
+  });
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
   return (
     <Layer column={columns}>
-      <RoomItem />
-      <RoomItem />
-      <RoomItem />
-      <RoomItem />
-      <RoomItem />
-      <RoomItem />
+      {data?.map((item, key) => {
+        return <RoomItem data={item} key={key} />;
+      })}
     </Layer>
   );
 };
